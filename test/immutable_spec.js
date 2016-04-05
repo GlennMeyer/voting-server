ddb49eafe7ff1cd1168c7b51ddedb916a328a679
Glenn Meyer <gmeyer@metalnetworks.com>
2016-04-04 22:03:28 -0500

    wrote tests for a tree

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -2,8 +2,20 @@ import {expect} from 'chai';
 import {List} from 'immutable';
 
 describe('immutability', () => {
+	describe('a number', () => {
 
-	// ...
+		function increment(currentState) {
+			return currentState + 1;
+		}
+
+		it('is immutable', () => {
+			let state = 42;
+			let nextState = increment(state);
+
+			expect(nextState).to.equal(43);
+			expect(state).to.equal(42);
+		});
+	});
 
 	describe('A List', () => {
 		function addMovie(currentState, movie) {
@@ -27,18 +39,33 @@ describe('immutability', () => {
 		});
 	});
 
-	describe('a number', () => {
-
-		function increment(currentState) {
-			return currentState + 1;
+	describe('A Tree', () => {
+		function addMovie(currentState, movie) {
+			return currentState.set(
+				'movies',
+				currentState.get('movies').push(movie)
+			);
 		}
 
 		it('is immutable', () => {
-			let state = 42;
-			let nextState = increment(state);
+			let state = Map({
+				movies: List.of('Trainspotting', '28 Days Later')
+			});
+			let nextState = addMovie(state, 'Sunshine');
 
-			expect(nextState).to.equal(43);
-			expect(state).to.equal(42);
+			expect(nextState).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later',
+					'Sunshine'
+				)
+			}));
+			expect(state).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later'
+				)
+			}));
 		});
 	});
 });
