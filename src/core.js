6d4655fb8aecd42e9ab987d82a93a0f90b96661b
Glenn Meyer <gmeyer@metalnetworks.com>
2016-04-05 19:11:23 -0500

    wrote tests for voting functionality

--- src/core.js
+++ src/core.js
@@ -1,3 +1,21 @@
-export function setEntires(state, entries) {
-	return state.get('entries', entries);
+import {List} from 'immutable';
+
+export function setEntries(state, entries) {
+	return state.get('entries', List(entries));
+}
+
+export function next(state) {
+	const entries = state.get('entries');
+	return state.merge({
+		vote: Map({pair: entries.take(2)}),
+		entries: entries.skip(2)
+	});
+}
+
+export function vote(state, entry) {
+	return state.updateIn(
+		['vote', 'tally', entry],
+		0,
+		tally => tally + 1
+	);
 }
